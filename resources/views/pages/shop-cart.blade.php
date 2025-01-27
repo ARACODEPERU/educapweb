ce7c27b5678b5c7fd7b6b15735b4a9ce64b58652
Yisus Web <connexion.jesus@gmail.com>
2025-01-27 11:34:12 -0500

    cart

--- resources/views/pages/shop-cart.blade.php
+++ resources/views/pages/shop-cart.blade.php
@@ -25,14 +25,15 @@
       <div class="section-content">
         <div class="row">
           <div class="col-md-8">
+            <span>0 programas en el carrito.</span>
             <div class="table-responsive">
               <table class="table table-striped table-bordered tbl-shopping-cart">
                 <thead>
                   <tr>
-                    <th></th>
-                    <th>Imagen</th>
+                    <th style="width: 5px;"></th>
+                    <th style="width: 30px;">Imagen</th>
                     <th>Producto</th>
-                    <th>Precio</th>
+                    <th style="width: 135px;">Precio</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -68,40 +69,66 @@
                                 <li class="variation-size">Categoria</li>
                             </ul>
                         </td>
-                        <td class="product-price"><span class="amount">S/ 50.00</span></td>
+                        <td class="product-price">
+                          <span class="amount">S/ 50.00</span>
+                        </td>
                     </tr>
                     <tr class="cart_item">
-                        <td class="product-remove"><a title="Remove this item" class="remove" href="#">×</a></td>
-                        <td class="product-thumbnail"><a href="#"><img alt="product" src="images/resource/products/3.jpg"></a></td>
-                        <td class="product-name"><a href="shop-product-details.php">Blue Solid Casual Shirt</a>
-                        <ul class="variation">
-                            <li class="variation-size">Size: <span>Large</span></li>
-                        </ul></td>
-                        <td class="product-price"><span class="amount">$68.00</span></td>
+                        <td class="product-remove">
+                          <a title="Remove this item" class="remove" href="#">×</a>
+                        </td>
+                        <td class="product-thumbnail">
+                          <a href="#">
+                            <img alt="product" src="{{ asset('themes/webpage/images/resource/products/3.jpg') }}">
+                          </a>
+                        </td>
+                        <td class="product-name">
+                          <a href="" style="font-size: 0.7rem;">Título del curso</a>
+                          <ul class="variation">
+                              <li class="variation-size">Categoria</li>
+                          </ul>
+                        </td>
+                        <td class="product-price">
+                          <span class="amount">S/ 50.00</span>
+                        </td>
                     </tr>
                 </tbody>
               </table>
             </div>
           </div>
           <div class="col-md-4">
-            <h4>Calculate Shipping</h4>
+            <h4>Datos del comprador</h4>
             <form class="form" action="#">
               <div class="mb-10">
+                <input type="text" class="form-control" placeholder="Nombres" value="">
+              </div>
+              <div class="mb-10">
+                <input type="text" class="form-control" placeholder="Apellido Paterno" value="">
+              </div>
+              <div class="mb-10">
+                <input type="text" class="form-control" placeholder="Apellido Materno" value="">
+              </div>
+              <div class="mb-10">
                 <select class="form-control">
-                  <option>Select Country</option>
-                  <option>Australia</option>
-                  <option>UK</option>
-                  <option>USA</option>
+                  <option>Tipo de documento</option>
+                  <option>DNI</option>
+                  <option>RUC</option>
+                  <option>Doc.trib.no.dom.sin.ruc</option>
                 </select>
               </div>
               <div class="mb-10">
-                <input type="text" class="form-control" placeholder="State/country" value="">
+                <input type="text" class="form-control" placeholder="N° Documento" value="">
               </div>
               <div class="mb-10">
-                <input type="text" class="form-control" placeholder="Postcod/zip" value="">
+                <input type="text" class="form-control" placeholder="Teléfono" value="">
+              </div>
+              <div class="mb-10">
+                <input type="text" class="form-control" placeholder="Correo electrónico" value="">
               </div>
               <div class="mb-30">
-                <button type="button" class="theme-btn btn-style-one"><span class="btn-title">Update Totals</span></button>
+                <button type="button" class="theme-btn btn-style-one">
+                  <span class="btn-title">Crear cuenta</span>
+                </button>
               </div>
             </form>
           </div>
