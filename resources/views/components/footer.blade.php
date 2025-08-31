092b8b693b4b4dec48e3c32508613b3d502c4687
Yisus Web <connexion.jesus@gmail.com>
2025-08-30 20:56:09 -0500

    ghgh

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,71 @@
 <div>
+
+    
+    <div id="whatsapp">
+        <a target="_blank" href="https://wa.link/jibpow" class="wtsapp">
+            <i class="fab fa-whatsapp"></i>
+        </a>
+    </div>
+
+    <style type="text/css">
+        #whatsapp .wtsapp {
+            position: fixed;
+            transform: all .5s ease;
+            background-color: #25D366;
+            display: block;
+            text-align: center;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+            border-radius: 50px;
+            border-right: none;
+            color: #fff;
+            font-weight: 700;
+            font-size: 30px;
+            bottom: 80px;
+            right: 20px;
+            border: 0;
+            z-index: 9999;
+            width: 50px;
+            height: 50px;
+            line-height: 50px;
+        }
+
+        #whatsapp .wtsapp:before {
+            content: "";
+            position: absolute;
+            z-index: -1;
+            left: 50%;
+            top: 50%;
+            transform: translateX(-50%) translateY(-50%);
+            display: block;
+            width: 60px;
+            height: 60px;
+            background-color: #25D366;
+            border-radius: 50%;
+            -webkit-animation: pulse-border 1500ms ease-out infinite;
+            animation: pulse-border 1500ms ease-out infinite;
+        }
+
+        #whatsapp .wtsapp:focus {
+            border: none;
+            outline: none;
+        }
+
+        @keyframes pulse-border {
+            0% {
+                transform: translateX(-50%) translateY(-50%) translateZ(0) scale(1);
+                opacity: 1;
+            }
+
+            100% {
+                transform: translateX(-50%) translateY(-50%) translateZ(0) scale(1.5);
+                opacity: 0;
+            }
+        }
+    </style>
+
     <footer class="main-footer"
         style="background: #000000;
-background: linear-gradient(0deg, rgba(0, 0, 0, 1) 0%, rgba(5, 10, 39, 1) 35%, rgba(5, 10, 39, 1) 100%);">
+                background: linear-gradient(0deg, rgba(0, 0, 0, 1) 0%, rgba(5, 10, 39, 1) 35%, rgba(5, 10, 39, 1) 100%);">
 
 
         <!--Widgets Section-->
